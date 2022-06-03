ae41f50dc3a58019a83c3f450f99bed6ba9e25fe
PercTruman <drshawnhart@gmail.com>
2022-06-02 21:43:50 -0500

    getting initial render

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
+
 
 function App() {
+  const [sushi, setSushi]=useState([])
+
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/sushis?_limit=4")
+      .then(res=>res.json())
+      .then(sushi=>setSushi(sushi));
+},[])
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushi={sushi}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({sushi}) {
+  const sushiSet =  sushi.map(item=>
+    <Sushi key={item.id} name={item.name} price={item.price} image={item.img_url}/>)
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushiSet}
       <MoreButton />
     </div>
   );
