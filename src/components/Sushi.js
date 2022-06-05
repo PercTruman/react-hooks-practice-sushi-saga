85a7aedd2ad4c069b9ef84cb418f0f226976abb7
PercTruman <drshawnhart@gmail.com>
2022-06-04 21:12:56 -0500

    seriously, I can make sushi disappear

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -6,11 +6,12 @@ function Sushi({ name, price, image}) {
   function toggleIsVisible(){
     setIsVisible(isVisible=>!isVisible)
   }
+  const visible = isVisible? <img src={image} alt={name} width="100%" />:null;
 
   return (
     <div className="sushi">
       <div className="plate" onClick={toggleIsVisible}>
-        {false? null:<img src={image} alt={name} width="100%" /> }
+        {visible}
       </div>
       <h4 className="sushi-details">
         {name} - ${price}
